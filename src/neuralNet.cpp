3ff0c3c6a4c3306f7ae93bb0f437fc93add56a5b
marco belli <elcabesa@inwind.it>
2020-09-17 22:48:35 +0200

    add option parsing

--- src/neuralNet.cpp
+++ src/neuralNet.cpp
@@ -2,7 +2,7 @@
 #include <random>
 #include <vector>
 
-
+#include "optparse/cxxopts.hpp"
 #include "labeledExample.h"
 #include "model.h"
 #include "linear.h"
@@ -15,96 +15,95 @@
 #include "memoryInputSet.h"
 #include "gradientDescend.h"
 
-
-int main(int argc, char*argv[]) {
-    double learnRate = 1e-4;
-    double regularization = 1.0;
-    double beta = 0.9;
-    unsigned int passes = 30000;
-    
-    if(argc == 5) {
-        passes = atoi(argv[1]);
-        learnRate = atof(argv[2]);
-        regularization = atof(argv[3]);
-        beta = atof(argv[4]);
-        std::cout<<"passes "<<passes<<std::endl;
-        std::cout<<"learnRate "<<learnRate<<std::endl;
-        std::cout<<"regularization "<<regularization<<std::endl;
-        std::cout<<"beta "<<beta<<std::endl;
-    }
-    
-    std::cout << "NeuralNET" << std::endl;
-    
+DiskInputSet getInputSet() {
     std::cout<<"read testset"<<std::endl;
+    
     DiskInputSet inSet("./TESTSET", 81920);
     inSet.generate();
+    
     std::cout<<"done"<<std::endl;
-    /*auto &valSet = inSet.validationSet();
-    for(auto& ex: valSet) {
-        std::cout<<ex->label()<<" - ";
-        ex->features().print();
-    }
     
-    for(unsigned int i =0; i<10; ++i) {
-        auto &valSet = inSet.batch();
-        for(auto& ex: valSet) {
-            std::cout<<ex->label()<<" - ";
-            ex->features().print();
-        }
-    }
-    return 0;*/
-    //inSet.printStatistics();
-    //return 0;
-    
+    return inSet;
+}
+
+Model createModel() {
     std::cout<<"creating model"<<std::endl;
+    
     Model m;
     m.addLayer(std::make_unique<ParallelDenseLayer>(2, 40960, 256, ActivationFactory::create(ActivationFactory::type::linear),1.0/sqrt(6)));
     m.addLayer(std::make_unique<DenseLayer>(512,32, ActivationFactory::create(ActivationFactory::type::relu)));
     m.addLayer(std::make_unique<DenseLayer>(32,32, ActivationFactory::create(ActivationFactory::type::relu)));
     m.addLayer(std::make_unique<DenseLayer>(32, 1, ActivationFactory::create(ActivationFactory::type::linear)));
-    std::cout<<"done"<<std::endl;
-    /*double maxValue = 1.e30;
-    for( int i = 0; i< 100; ++i ) {
-        m.randomizeParams();
-        double loss = m.calcAvgLoss(inSet.validationSet());
-        std::cout<<"final total loss: " <<loss<<std::endl;    
-        if( loss < maxValue) {testSet = generateTestSetList()
-            maxValue =loss;
-            std::cout<<"NEW MAX VALUE: " <<maxValue<<std::endl;  
-            
-                std::cout<<"serialize"<<std::endl;
-                std::ofstream nnFile;
-                nnFile.open ("nn.txt");
-                m.serialize(nnFile);
-                nnFile.close();
-                std::cout<<"done"<<std::endl;
-            
-        }
-    }
-    //m.printParams();
-    std::cout<<"randomized params"<<std::endl;*/
     
-    std::cout<<"reload"<<std::endl;
+    std::cout<<"done"<<std::endl;
+    
+    return m;
+}
+
+
+int main(int argc, const char*argv[]) {
+    
+    
+    cxxopts::Options options("Neural Net", "Vajolet Neural net trainer");
+    options.add_options()
+        ("help", "Print help")
+        ("p,passes", "traiing passes", cxxopts::value<unsigned int>()->default_value("30000"))
+        ("e,eta", "eta", cxxopts::value<double>()->default_value("1e-4"))
+        ("r,regularization", "regularization value", cxxopts::value<double>()->default_value("1.0"))
+        ("b,beta", "rmsprop beta", cxxopts::value<double>()->default_value("0.9"))
+        ("randomize", "randomize model parmeters", cxxopts::value<bool>()->default_value("false"))
+        ("n,nPath", "weight file path", cxxopts::value<std::string>()->default_value("./nn-start.txt"))
+    ;
+    
+    auto result = options.parse(argc, argv);
+    
+    if (result.count("help"))
     {
+      std::cout << options.help({"", "Group"}) << std::endl;
+      exit(0);
+    }
+    
+    
+    std::cout << "NeuralNET" << std::endl;
+    
+    
+    auto inSet = getInputSet();
+    
+    auto m = createModel();
+    
+    if (result["randomize"].as<bool>()) {
+        m.randomizeParams();
+        std::cout<<"randomized params"<<std::endl;
+        std::cout<<"serialize"<<std::endl;
+        std::ofstream nnFile;
+        nnFile.open ("nn-start.txt");
+        m.serialize(nnFile);
+        nnFile.close();
+        std::cout<<"done"<<std::endl;
+    }
+    
+    if (result.count("nPath")) {
         std::cout<<"deserialize"<<std::endl;
         std::ifstream nnFile;
-        nnFile.open ("nn-start.txt");
+        nnFile.open (result["nPath"].as<std::string>());
         if(m.deserialize(nnFile)){
              std::cout<<"done"<<std::endl;
         }else {
              std::cout<<"FAIL"<<std::endl;
+             exit(-1);
         }
         nnFile.close();
-        //m.printParamsStats();
     }
-    //exit(0);
 
-    GradientDescend gd(m, inSet, passes, learnRate, regularization, beta);
+    GradientDescend gd(m,
+                       inSet,
+                       result["passes"].as<unsigned int>(),
+                       result["eta"].as<double>(),
+                       result["regularization"].as<double>(),
+                       result["beta"].as<double>()
+                      );
     
     gd.train();
-    
-
-    std::cout<<"-------------------------"<<std::endl;
 
     {
         std::cout<<"serialize"<<std::endl;
@@ -114,23 +113,6 @@ int main(int argc, char*argv[]) {
         nnFile.close();
         std::cout<<"done"<<std::endl;
     }
-    //std::cout<<"randomize Params"<<std::endl;
-    //m.randomizeParams();*/
-    /*std::cout<<"final total loss: " <<m.calcAvgLoss(inSet.validationSet())<<std::endl;
-    std::cout<<"reload"<<std::endl;
-    {
-        std::cout<<"deserialize"<<std::endl;
-        std::ifstream nnFile;
-        nnFile.open ("nn.txt");
-        if(m.deserialize(nnFile)){
-             std::cout<<"done"<<std::endl;
-        }else {
-             std::cout<<"FAIL"<<std::endl;
-        }
-        nnFile.close();
-    }
-    std::cout<<"final total loss: " << m.calcAvgLoss(inSet.validationSet())<<std::endl;*/
-    
 }
 
 
